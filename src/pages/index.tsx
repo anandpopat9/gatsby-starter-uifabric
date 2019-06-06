c6bcb6e11600ce912ded6fda0abc634a268e10bf
Anand <anandpopat9@gmail.com>
2019-06-06 00:07:23 -0400

    temp pdf

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,66 +1,30 @@
-import React from "react"
-import { Stack, Text, Link, FontWeights } from "office-ui-fabric-react"
-import "./index.css"
+import React, { Component } from 'react';
+import { Document, Page } from 'react-pdf';
+import "./index.css";
+class App extends Component {
+  state = {
+    numPages: null,
+    pageNumber: 1,
+  }
 
-const boldStyle = { root: { fontWeight: FontWeights.semibold } }
+  onDocumentLoadSuccess = ({ numPages }) => {
+    this.setState({ numPages });
+  }
 
-const App: React.FunctionComponent = () => {
-  return (
-    <Stack
-      horizontalAlign="center"
-      verticalAlign="center"
-      verticalFill
-      styles={{
-        root: {
-          width: "960px",
-          margin: "0 auto",
-          textAlign: "center",
-          color: "#605e5c",
-        },
-      }}
-      className="App"
-      gap={15}
-    >
-      <img
-        src="https://raw.githubusercontent.com/Microsoft/just/master/packages/just-stack-uifabric/template/src/components/fabric.png"
-        alt="logo"
-      />
-      <Text variant="xxLarge" styles={boldStyle}>
-        Welcome to Your UI Fabric App
-      </Text>
-      <Text variant="large">
-        For a guide on how to customize this project, check out the UI Fabric
-        documentation.
-      </Text>
-      <Text variant="large" styles={boldStyle}>
-        Essential Links
-      </Text>
-      <Stack horizontal gap={15} horizontalAlign="center">
-        <Link href="https://developer.microsoft.com/en-us/fabric">Docs</Link>
-        <Link href="https://stackoverflow.com/questions/tagged/office-ui-fabric">
-          Stack Overflow
-        </Link>
-        <Link href="https://github.com/officeDev/office-ui-fabric-react/">
-          Github
-        </Link>
-        <Link href="https://twitter.com/officeuifabric">Twitter</Link>
-      </Stack>
-      <Text variant="large" styles={boldStyle}>
-        Design System
-      </Text>
-      <Stack horizontal gap={15} horizontalAlign="center">
-        <Link href="https://developer.microsoft.com/en-us/fabric#/styles/icons">
-          Icons
-        </Link>
-        <Link href="https://developer.microsoft.com/en-us/fabric#/styles/typography">
-          Typography
-        </Link>
-        <Link href="https://developer.microsoft.com/en-us/fabric#/styles/themegenerator">
-          Theme
-        </Link>
-      </Stack>
-    </Stack>
-  )
+  render() {
+    const { pageNumber, numPages } = this.state;
+
+    return (
+      <div>
+        <Document
+          file="Resume.pdf"
+          onLoadSuccess={this.onDocumentLoadSuccess}
+        >
+          <Page pageNumber={pageNumber} />
+        </Document>
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
